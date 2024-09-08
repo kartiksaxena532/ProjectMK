7479852a483b8ef21551138f51e888d839b5a679
Kartik Saxena <karrtiksaxena@gmail.com>
2024-09-09 02:08:34 +0530

    added new hero robo

--- src/components/Hero/heros/HeroRobo.jsx
+++ src/components/Hero/heros/HeroRobo.jsx
@@ -1,8 +1,8 @@
-import Robo from '../../../assets/images/model_hero_variation.png'
+import Robo from '../../../assets/images/3d model_transparent_Craiyon.png'
 
 const HeroRobo = () => {
   return (
-    <div className=" mt-[4.5rem] h-[90vh] w-[43vw] align-right ">
+    <div className=" mt-[4rem] h-[91vh] w-[38vw] align-right ">
     <img src={Robo} alt="Logo" className="w-full h-full transform -scale-x-100 -z-999" />
 </div>
   )
