05b71ab51c0c69eb8d1e27a009cc1409e6620ebe
Kartik Saxena <karrtiksaxena@gmail.com>
2024-09-10 15:01:18 +0530

    added side card

--- src/components/Hero/card/TransCard.jsx
+++ src/components/Hero/card/TransCard.jsx
@@ -1,8 +1,11 @@
-
+import card from "../../../assets/SmallOulineHero.svg"
 
 const TransCard = () => {
   return (
-    <div className="flex w-60 h-60 rounded-xl backdrop-blur-sm ring-2 ring-white"></div>
+    <div className="flex w-56 h-64 rounded-xl backdrop-blur-sm">
+      <div className="rounded-xl w-48 h-28 mt-12 mx-auto bg-zinc-800 opacity-30 items-center flex justify-center "></div>
+      <img src={card} alt="Logo" className="absolute -z-99 w-64 h-64 fill-white " />
+    </div>
   )
 }
 
--- src/components/Hero/heros/HeroRobo.jsx
+++ src/components/Hero/heros/HeroRobo.jsx
@@ -3,9 +3,10 @@ import TransCard from '../card/TransCard'
 
 const HeroRobo = () => {
   return (
-    <div className='flex'>
-    <div className="mt-[8rem]  h-[87vh] w-[32vw] align-right  ">
+    <div className='flex  flex-row'>
+    <div className="mt-[8rem] h-[87vh] w-[32vw] align-right   ">
     <img src={Robo} alt="Logo" className=" absolute -ml-36 transform -scale-x-100 -z-999 scale-125 w-[40vw]  h-[40vw]" />
+    <p className='flex justify-end rotate-90 mt-20 ml-56 text-3xl font-thin'>Beyond the horizon</p>
 </div>
 <div className='absolute flex bottom-20 right-60'>
 <TransCard/>
