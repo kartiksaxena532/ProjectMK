300dc2c11f4fd8874080f7ec22fb87a19dd10127
Kartik Saxena <karrtiksaxena@gmail.com>
2024-09-14 15:07:23 +0530

    added robo card

--- src/components/Hero/card/TransCard.jsx
+++ src/components/Hero/card/TransCard.jsx
@@ -1,11 +1,14 @@
 import card from "../../../assets/SmallOulineHero.svg"
-
+import image from "../../../assets/transcard.jpg"
 const TransCard = () => {
   return (
-    <div className="flex w-56 h-64 rounded-xl backdrop-blur-sm">
-      <div className="rounded-xl w-48 h-28 mt-12 mx-auto bg-zinc-800 opacity-30 items-center flex justify-center "></div>
-      <img src={card} alt="Logo" className="absolute -z-99 w-64 h-64 fill-white " />
+    <>
+    <div className="w-56 h-64 rounded-xl backdrop-blur flex justify-center flex-col gap-2 items-center">
+      <div className="rounded-xl w-48 h-28 mx-auto items-center flex justify-center overflow-hidden ring-1 ring-indigo-100"> <img src={image} alt="Logo" className=" translate-y-8 " /></div>
+      <p className=" text-md font-bold tracking-wide">Multi-Sensor Humanoid<br></br>Robot For War</p>
+      <img src={card} alt="Logo" className="absolute -z-[999] w-64 h-64 fill-white backdrop-blur-sm"/>
     </div>
+     </>
   )
 }
 
--- src/components/Hero/heros/HeroRobo.jsx
+++ src/components/Hero/heros/HeroRobo.jsx
@@ -1,12 +1,12 @@
-import Robo from '../../../assets/images/3d model_transparent_Craiyon.png'
+import Robo from '../../../assets/images/hero.png'
 import TransCard from '../card/TransCard'
 
 const HeroRobo = () => {
   return (
     <div className='flex  flex-row'>
-    <div className="mt-[8rem] h-[87vh] w-[32vw] align-right   ">
+    <div className="mt-[15rem] h-[87vh] w-[32vw] align-right   ">
     <img src={Robo} alt="Logo" className=" absolute -ml-36 transform -scale-x-100 -z-999 scale-125 w-[40vw]  h-[40vw]" />
-    <p className='flex justify-end rotate-90 mt-20 ml-56 text-3xl font-thin'>Beyond the horizon</p>
+    <p className='flex justify-end rotate-90 mt-20 ml-56 text-3xl font-regular'>Beyond the horizon</p>
 </div>
 <div className='absolute flex bottom-20 right-60'>
 <TransCard/>
