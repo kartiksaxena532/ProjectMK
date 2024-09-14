dce5e8e3e78d9ab1f57a5ffe46a59eb2cbfb872c
Kartik Saxena <karrtiksaxena@gmail.com>
2024-09-14 15:54:21 +0530

    added long vard

--- src/components/Hero/heros/HeroLongCard.jsx
+++ src/components/Hero/heros/HeroLongCard.jsx
@@ -1,9 +1,15 @@
-
+import base1 from "../../../assets/E1smallOuline.svg"
+import base0 from "../../../assets/EsmallOutlineHero.svg"
+import base2 from "../../../assets/E2SmallOuline.svg"
 
 const HeroLongCard = () => {
   return (
-    <div className="ring-2 h-[75vh] w-[17vw]">
-        Kartik
+    <div className="ring-2 h-[75vh] w-[17vw] ">
+      <div className="flex flex-col  justify-start items-center">
+       <img src={base0} alt="base0" className="absolute mt-28 "/>
+       <img src={base1} alt="base1"  className="absolute mt-12 "/>
+        <img src={base2} alt="base2"  className="absolute"/>
+        </div>
     </div>
   )
 }
