ad94926464a9b91110c16bd8ee81815cc2c6a8b5
Kartik Saxena <karrtiksaxena@gmail.com>
2025-11-03 00:12:19 +0530

    bento updated

--- src/components/Page2/Bento.jsx
+++ src/components/Page2/Bento.jsx
@@ -1,89 +1,73 @@
 import { useRef } from 'react';
-import song from '../../assets/page2/song.mp3'; 
-    import Old from "../../assets/page2/old.png"
-    import Logo from "../../assets/page2/avatar.png"
-    import Young from "../../assets/page2/young.png"
-    import plus from "../../assets/page2/plus.webp"
-    import Iphone from "../../assets/page2/mockip.webp"
-    import DVA  from "../../assets/images/Mk_Logo.png"
-  
-    const Bento = () => {
-      const audioRef = useRef(null);
+import song from '../../assets/page2/song.mp3';
+import Old from "../../assets/page2/old.png"
+import Logo from "../../assets/page2/avatar.png"
+import Young from "../../assets/page2/young.png"
+import Logo1 from "../../assets/images/one.png"
+import Logo2 from "../../assets/images/two.png"
+import Logo3 from "../../assets/images/three.png"
+import Iphone from "../../assets/images/Stop.png"
+import DVA from "../../assets/images/Mk_Logo.png"
+import Gif from "../../assets/images/Gif.mp4"
+const Bento = () => {
+  const audioRef = useRef(null);
 
-      // Function to play the audio on hover
-      const handleMouseEnter = () => {
-        if (audioRef.current) {
-          audioRef.current.play();
-        }
-      };
-    
-      // Function to pause and reset the audio on hover leave
-      const handleMouseLeave = () => {
-        if (audioRef.current) {
-          audioRef.current.pause();
-          audioRef.current.currentTime = 0; // Reset to start
-        }
-      };
-      return (
-        
-      <div className="hidden xl:px-48 lg:grid grid-cols-1 grid-rows-12 lg:grid-cols-4 lg:grid-rows-6 bg-dark p-8 gap-4">
+  // Function to play the audio on hover
+  const handleMouseEnter = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  // Function to pause and reset the audio on hover leave
+  const handleMouseLeave = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0; // Reset to start
+    }
+  };
+  return (
+
+    <div className="hidden xl:px-48 lg:grid grid-cols-1 grid-rows-12 lg:grid-cols-4 lg:grid-rows-6 bg-dark p-8 gap-4">
       <div className="relative col-span-1 row-span-4 rounded-xl bg-blue-100 overflow-hidden">
-        <h1 className="absolute font-semibold top-4 left-4 mr-12 text-xl text-black">
-          Develop together with mechanical speed and memory management.
+        <h1 className="absolute font-bold top-4 left-4 mr-12 text-xl text-black">Branding <br /> Ready Designs!
         </h1>
-        <img className="rounded-2xl absolute -scale-x-100 -bottom-16" src={Old} alt="old" />
+        <img className="rounded-2xl absolute hover:-translate-y-4 -bottom-4 " src={Old} alt="old" />
       </div>
-      <div className="grid  grid-rows-1 grid-cols-4 col-span-2 row-span-1 border border-white/50 rounded-xl">
-        <div className=" bg-red-100 text-red-100 rounded-bl-xl rounded-tl-xl">
-          .
+      <div className="grid  grid-rows-1 grid-cols-4 col-span-2 row-span-1 border overflow-hidden border-white/50 rounded-xl">
+        <div className=" bg-red-100 text-black text-2xl text-center py-4 font-yatra rounded-bl-xl rounded-tl-xl">
+          Logo <br></br>Design
         </div>
-        <div className="bg-yellow-200 text-yellow-200">.</div>
-        <div className="bg-blue-600 text-blue-600">.</div>
-        <div className="bg-green-500  text-green-500 rounded-tr-xl rounded-br-xl ">
-          .
+        <div className="bg-black-200 h-8"><img src={Logo1} className='-translate-y-5 object-fit'></img></div>
+        <div className="bg-black-200 h-8"><img src={Logo2} className=' scale-105 object-fit'></img></div>
+        <div className="bg-black-200 h-8 rounded-tr-xl rounded-br-xl"><img src={Logo3} className='-translate-y-4 object-fit'></img>
         </div>
       </div>
-      <div className="relative bg-blue-500 col-span-1 row-span-2 rounded-xl">
-        <h1 className="absolute  left-4 flex text-9xl top-4">87+</h1>
+      <div className="relative bg-blue-500 col-span-1 row-span-2 rounded-xl overflow-hidden">
+        <video
+          loop
+          autoPlay
+          muted
+          src={Gif}
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
         <div className="absolute pt-12  bottom-4 left-4 flex flex-row items-center">
-          <img
-            className="bg-yellow-200 rounded-full"
-            src={Old}
-            width={20}
-            height={20}
-            alt="old"
-          />
-          <img 
-            className="bg-red-200 rounded-full"
-            src={Old}
-            width={20}
-            height={20}
-            alt="old"
-          />
-          <img 
-            className="bg-blue-200 rounded-full"
-            src={plus}
-            width={30}
-            height={30}
-            alt="plus"
-          />
-          <p className="text-xs px-1">
-            Tech related futuristic designs implemented!
+          <p className="text-xl px-4 text-center font-semibold text-white bg-blue-400 rounded-md">
+            Custom Animations
           </p>
         </div>
       </div>
       <div className="relative bg-gradient-to-t from-green-100 via-yellow-100 to-pink-100 col-span-2 row-span-3  rounded-xl">
         <span className="absolute text-sm font-semibold text-black left-4 top-4 hover:text-black hover:text-lg">
-          <a href="https://kartikdesign.netlify.app" >
-          Check Out Design Language
+          <a href="https://kartikdesign.netlify.app" className='text-blue-400 font-yatra underline animate-bounce' >
+            Check Out My Portfolio !
           </a>
         </span>
-        <h1 className="absolute mt-3 font-yatra items-center justify-center left-4 top-12 text-6xl  text-zinc-600 font-thin">
-          Bento Grid <br /> Coded To <br /> Up the Design.
-        </h1>
-        <span className="absolute  bottom-4 left-4  text-sm  text-black ">
-          Developed together with <br /> science based robotics <br />{" "} 
-          experts to get fine results!
+        <span className="absolute font-mons items-center justify-center left-4 top-12 text-6xl  text-zinc-600 font-thick">
+         Sky is our limit! <span className=' italic font-yatra' /> Fresh & Cool <span /><br></br> Brand Assets!
+        </span>
+        <span className="absolute  bottom-4 left-4 italic text-sm  text-black ">
+          We{`'`}ve created a <span className='font-yatra text-xl italic'>Variety of Assets </span><br />for <span className='font-yatra text-xl italic'>different</span> brands {" "} with <span className='font-yatra text-xl italic'>quality</span> work under deadlines!
         </span>
         <img
           src={Logo}
@@ -91,33 +75,34 @@ import song from '../../assets/page2/song.mp3';
           height={50}
           className="absolute bottom-4
           right-4"
-            alt="Logo"
+          alt="Logo"
         />
       </div>
       <div className="relative col-span-1 row-span-4 rounded-xl bg-blue-100">
         <p className="absolute text-xl  left-4 top-4 text-black ">
-          Karvik <br /> Redefined, <br />
-          by AI and Web.
+          Redefining <br />Website And <br>
+          </br>Application Interface Design.
         </p>
         <button className="px-3 right-4 top-6 absolute  text-sm py-1 border-blue-500 text-blue-500 border rounded-2xl cursor-pointer hover:bg-blue-500 hover:text-white duration-700">
           {" "}
-          Enter{" "}
+          有門{" "}
         </button>
         <img src={Young} className="absolute bottom-0 rounded-xl" alt="young" />
       </div>
       <div className="relative col-span-1 row-span-2 bg-blue-500 rounded-xl">
-        <button className="absolute text-sm rounded-xl left-2 top-2 px-2 py-1 bg-white text-blue-500 hover:bg-red-400 hover:text-light duration-500" >Top Rated Courses!</button>
-        <h1 className="absolute right-4 top-12 text-5xl" >95.6%</h1>
-        <img src={Iphone} width={120} className="absolute bottom-0 right-0" alt="young" />
+        <button className="absolute text-sm rounded-xl left-2 top-2 px-2 py-1 bg-white text-blue-500 hover:bg-red-400 hover:text-light duration-500 font-yatra" >Stickers</button>
+        <img src={Iphone} width={120} className="mx-auto my-10 object-fit rounded-xl" alt="young" />
+        <h1 className='absolute font-bold bottom-4 pl-16 italic text-xl text-center'>Sticker Design</h1>
       </div>
       <div className="relative items-center justify-center col-span-2 row-span-2 bg-green-400 rounded-xl">
-       
-          <img className="mx-auto justify-center items-center rounded-full m-3 h-48 w-48 hover:animate-spin" src={DVA}   onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}  alt="young" />
-          <audio ref={audioRef} src={song} />
+        <img className="mx-auto justify-center items-center rounded-full m-3 h-48 w-48 hover:animate-spin" src={DVA} onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave} alt="young" />
+        <p className="absolute top-4 left-6 text-xl italic font-yatra text-white font-semibold">Here Is Something..</p>
+        <p className="absolute bottom-4 right-6 text-xl italic font-yatra text-white font-semibold">Hover To Hear!</p>
+        <audio ref={audioRef} src={song} />
       </div>
     </div>
-      )
-    }
-    
-    export default Bento
+  )
+}
+
+export default Bento
--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -1,13 +1,14 @@
 
-
+import Email from '../../assets/images/icons/Email.png'
+import Insta from '../../assets/images/icons/Insta.png'
+import In from '../../assets/images/icons/linked.png'
 const FooterMain = () => {
   return (
     <div className="flex flex-col ">
       <div className="flex justify-center rounded-full ring-4 ring-fuchsia-100">
-      </div>
-      <div className="flex h-[45vh] align-center justify-center text-[7vw] uppercase font-bold font-mons">
+      </div>      <div className="flex h-[45vh] align-center justify-center text-[7vw] uppercase font-bold font-mons">
         <div className="flex justify-center items-center">
-          <div className="bg-transparent gap-16 md:gap-28 font-mons ">Let{`'`}s Work Together!
+          <div className="bg-transparent gap-16 md:gap-28 font-mons ">Let{`'`}s Work <span className="text-green-500 ml-4 bg-white px-2">Together</span>
           </div>
         </div>
       </div>
@@ -16,15 +17,16 @@ const FooterMain = () => {
           <div className="flex justify-center items-center text-white text-2xl md:text-4xl font-mons font-light mt-10">Graphich Design | Branding | Logo Design | UX/UI Design
           </div>
           <div className="flex flex-row justify-between font-sans items-center w-[90vw] mt-10">
-            <div className="flex justify-start items-center text-white text-sm md:text-lg font-bold font-mons ">Contact Us
+            <div className="flex justify-start items-center text-white text-sm italic md:text-2xl font-bold font-mons ">Contact Us
             </div>
             <div className="flex justify-end flex-row gap-10 md:gap-20">
-              <div className="flex justify-center items-center font-sans text-white text-sm md:text-lg">FB
-              </div>
-              <div className="flex justify-center items-center text-white text-sm md:text-lg font-sans ">Insta
-              </div>
-              <div className="flex justify-center items-center text-white text-sm md:text-lg font-sans ">Twitter
-              </div>
+              <a href="mailto:karrtiksaxena@gmail.com" className="flex justify-center items-center font-sans hover:animate-pulse text-white text-sm md:text-2xl">
+                <img src={Email} alt="email" className="w-8 h-8 invert" />
+              </a>
+              <a href="" className="flex justify-center items-center text-white  md:text-2xl font-sans hover:animate-pulse"> <img src={Insta} alt="email" className="w-8 h-8 invert" />
+              </a>
+              <a href="https://www.linkedin.com/company/may-kreate" className="flex justify-center items-center hover:animate-pulse text-white text-sm md:text-2xl font-sans "> <img src={In} alt="email" className="w-9 h-9  invert" />
+              </a>
             </div>
           </div>
         </div>
